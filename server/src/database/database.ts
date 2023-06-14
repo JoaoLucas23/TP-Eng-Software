2e5e0b2395089111085ab32794131b85da161e34
JoaoLucas23 <jllgoncalves23@gmail.com>
2023-06-14 16:23:46 -0300

    sqlite

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -2,17 +2,8 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-    'eletro_master',
-    'root',	
-    '#Jojoka07',
-    {
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      timezone: '-03:00',
-      pool: {
-        max: 10,
-        min: 0,
-        idle: 10000
-      }
-    },
+  {
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+  }
 );
